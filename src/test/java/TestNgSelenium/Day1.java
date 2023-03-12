2db9e623747b2b2061a9f7f10638bad12e329c37
AnkitaBehl <behlankita24@gmail.com>
2023-03-12 15:16:14 +0530

    first commit

---
+++ src/test/java/TestNgSelenium/Day1.java
@@ -0,0 +1,31 @@
+package TestNgSelenium;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+public class Day1 {
+
+	@AfterTest
+	public void lastececution()
+	{
+		System.out.println("I will Execute last");
+	}
+	@Test
+	
+	public void Demo()
+	{
+		System.out.println("Hello");
+	}
+	@AfterSuite
+	public void Afsuit()
+	{
+		System.out.println("I will be 1 last no ");
+	}
+  @Test
+  public void SecondTest()
+  {
+	  System.out.println("Bye");
+  }
+}
+
